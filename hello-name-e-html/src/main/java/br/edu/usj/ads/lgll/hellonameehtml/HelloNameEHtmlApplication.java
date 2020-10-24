af3c71655d44f746834714ddd6c651a0fd7be9dd
Luciana <eng.luciana.cabral@gmail.com>
2020-10-24 14:16:25 -0300

    Primeiro commit

---
+++ hello-name-e-html/src/main/java/br/edu/usj/ads/lgll/hellonameehtml/HelloNameEHtmlApplication.java
@@ -0,0 +1,13 @@
+package br.edu.usj.ads.lgll.hellonameehtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloNameEHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloNameEHtmlApplication.class, args);
+	}
+
+}
