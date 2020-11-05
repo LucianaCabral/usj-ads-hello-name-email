065a4fa6905f0a78d1d975100eb274b421123572
Luciana <eng.luciana.cabral@gmail.com>
2020-11-05 09:14:44 -0300

    First commit

---
+++ calculadora/src/main/java/br/edu/usj/ads/lgll/calculadora/Calculadora.java
@@ -0,0 +1,33 @@
+package br.edu.usj.ads.lgll.calculadora;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class Calculadora {
+
+    @PostMapping(value="/calcular")
+    public ModelAndView postCalcular(@RequestParam String numero1, @RequestParam String numero2, @RequestParam String operador) {
+        
+        ModelAndView modelAndView = new ModelAndView ("index");
+        Double resultado = 0.0;
+     
+        switch(operador) {
+            case "+": resultado = Double.parseDouble(numero1) + Double.parseDouble(numero2); break;
+            case "-": resultado = Double.parseDouble(numero1) - Double.parseDouble(numero2); break;
+            case "*": resultado = Double.parseDouble(numero1) * Double.parseDouble(numero2); break;
+            case "/": resultado = Double.parseDouble(numero1) / Double.parseDouble(numero2); break;
+          
+        }
+        
+        String texto = "Resultado: " + resultado;
+        modelAndView.addObject("mensagem", texto);
+    
+            return modelAndView;
+        }
+        
+        
+    }
